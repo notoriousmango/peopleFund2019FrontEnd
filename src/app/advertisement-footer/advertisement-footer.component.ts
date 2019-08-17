8b89f9e22d9b7fa0e7b2a1d4d944008b3f86ed73
Seong Min Park <psm03120@gmail.com>
2019-08-17 08:15:27 -0400

    footer and chat media done

--- src/app/advertisement-footer/advertisement-footer.component.ts
+++ src/app/advertisement-footer/advertisement-footer.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class AdvertisementFooterComponent implements OnInit {
   public footerTitleEnglish = 'Extraordinary Finance for the Ordinary';
   public footerTitleKorean = '보통 사람을 위한 보통이 아닌 금융을 만들겠습니다.';
-  
+  public mobileInvestStartButton = '1분만에 투자 시작하기';
   constructor() { }
 
   ngOnInit() {
